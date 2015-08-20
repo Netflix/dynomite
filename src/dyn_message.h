bf510610204eb738d1d6ac1e9524d4df762ef3c9
Shailesh Birari <sbirari@netflix.com>
2015-08-20 11:46:41 -0700

    Add req in the rspmgr for debugging and other purposes.

--- src/dyn_message.h
+++ src/dyn_message.h
@@ -226,15 +226,16 @@ struct response_mgr {
     /* we could use the dynamic array
        here. But we have only 3 ASGs */
     struct msg  *responses[MAX_REPLICAS_PER_DC];
-    uint8_t     received_responses; // non-error responses received
+    uint8_t     good_responses;     // non-error responses received
     uint8_t     max_responses;      // max responses expected
     uint8_t     quorum_responses;   // responses expected to form a quorum
     uint8_t     error_responses;    // error responses received
     struct msg  *err_rsp;           // first error response
     struct conn *conn;
+    struct msg *msg;
 };
 
-void init_response_mgr(struct response_mgr *rspmgr, bool is_read,
+void init_response_mgr(struct response_mgr *rspmgr, struct msg*, bool is_read,
                        uint8_t max_responses, struct conn *conn);
 // DN_OK if response was accepted
 rstatus_t rspmgr_submit_response(struct response_mgr *rspmgr, struct msg *rsp);
