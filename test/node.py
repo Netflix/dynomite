2d8c9c5ed153a85573327c0e9a5e6b9fba53303e
Shailesh <sbirari@netflix.com>
2015-12-17 18:11:43 -0800

    Basic functionality test framework
        o connects to an existing cluster (hardcoded)
        o does a dual write to redis and dynomite and raises
          assertion if results differ
        o runs basic key value, large payload and hash map test
        o connects to a random node in the cluster
        o Integrates with travis

---
+++ test/node.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+class Node(object):
+    def __init__(self, host="localhost", ip="127.0.0.1", port=1212):
+        self.host=host
+        self.port=port
+        self.name="Node: %s:%d" % (host, port)
+    def __name__(self):
+        return self.name
+    def start(self):
+        return
+    def stop(self):
+        return
