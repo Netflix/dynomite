b69ed2032037890353eab7991d9ca7d33dcf92d9
ipapapa <iduckhd@hotmail.com>
2016-03-03 10:38:07 -0800

    updates on launch script

--- bin/launch_dynomite.sh
+++ bin/launch_dynomite.sh
@@ -1,27 +1,77 @@
 #!/bin/bash
 
+
+# ** This check has to be after the redis check above.
+# Quit if Dynomite is already running, rather than
+# throwing errors from this script.
+if (ps -ef | grep  '[/]apps/dynomite/bin/dynomite'); then
+    logger -s "Dynomite already running, no need to restart it again."
+    exit 0
+fi
+
 DYN_DIR=/apps/dynomite
 LOG_DIR=/logs/system/dynomite
 CONF_DIR=$DYN_DIR/conf
 
-# this code lifted shamelessly from Mike T.'s /etc/init.d/tomcat
-# figure out the appropriate user that we should run by
-typeset -x `stat --printf "userowner=%U\ngroupowner=%G\n" $0`
-mkdir -p $LOG_DIR
-chown -R $userowner:nac  $LOG_DIR
 
-DEBUG_STR="-v0"
+#** Requires setting the EC2 Instance type as ENV variable
+# If Dynomite is used outside of AWS environment the 
+# following can be used as ideas on how much memory Dynomite
+# should take.
 
-if [ ! -z "$DYN_DEBUG_LEVEL" ] ; then
-   DEBUG_STR="-v$DYN_DEBUG_LEVEL"
+# Message allocation based on the instance type
+# 2GB for Florida + 85% for Redis (rest available for OS)
+if [ "$EC2_INSTANCE_TYPE" == "r3.xlarge" ]; then
+# r3.xlarge: 30.5GB RAM (2.5GB available)
+    ALLOC_MSGS=100000
+elif [ "$EC2_INSTANCE_TYPE" == "r3.2xlarge" ]; then
+# r3.2xlarge: 61GB RAM (7.15GB available)
+    ALLOC_MSGS=300000
+elif [ "$EC2_INSTANCE_TYPE" == "r3.4xlarge" ]; then
+# r3.4xlarge: 122GB RAM (16.3GB available)
+    ALLOC_MSGS=800000
+elif [ "$EC2_INSTANCE_TYPE" == "r3.8xlarge" ]; then
+# r3.8xlarge: 244GB RAM (34.19GB available)
+    ALLOC_MSGS=1600000
+fi
+echo "Instance Type: $EC2_INSTANCE_TYPE --> Allocated messages: $ALLOC_MSGS"
+
+
+declare -x `stat --printf "userowner=%U\ngroupowner=%G\n" "$0"`
+
+if [ ! -d "$LOG_DIR" ]; then
+    sudo mkdir -p $LOG_DIR
+    sudo chown -R $userowner:$groupowner $LOG_DIR
+fi
+
+#save the previous log
+if [ -e $LOG_DIR/dynomite.log ]; then
+    mv $LOG_DIR/dynomite.log $LOG_DIR/dynomite-$(date +%Y%m%d_%H%M%S).log
+fi
+
+
+if [ -z "$MBUF_SIZE" ]; then
+    echo "MBUF_SIZE is empty. Use default value 16K"
+    MBUF_SIZE=16384
+fi
+
+if [ -z "$ALLOC_MSGS" ]; then
+    echo "ALLOC_MSGS is empty. Use default value 200K"
+    ALLOC_MSGS=200000
 fi
 
 
 # note that we do not use 'su - username .... ' , because we want to keep the env settings that we have done so far
-cmd="$DYN_DIR/bin/dynomite -c $CONF_DIR/dynomite.yml -d --output=$LOG_DIR/dynomite.log  $DEBUG_STR  > $LOG_DIR/dynomite_start-$(date +%Y%m%d_%H:%M:%S).out 2>&1 &"
+cmd="$DYN_DIR/bin/dynomite -d -c $CONF_DIR/dynomite.yml -m$MBUF_SIZE -M$ALLOC_MSGS --output=$LOG_DIR/dynomite.log "
+
+
 if [ $USER != "root" ];then
     exec $cmd
 else
     su  $userowner -c "$cmd"
 fi
+
+sleep 1
+
+sudo $DYN_DIR/bin/core_affinity.sh
 
