aedc08888c0f204db919817d01f4a1e6d3f9b970
Sailesh Mukil <smukil@netflix.com>
2018-11-11 17:20:34 -0800

    Refactor test framework to easily expand on cluster management
    This patch takes a lot of the code from test/cluster_generator.py
    and refactors it into easily manageable classes which allows for
    easily creating and destroying a dynomite cluster in a local test
    environment. Refer to the last paragraph of this commit message for
    the motivation behind this patch.
    
    To create a dynomite cluster, now it's as simple as doing the following:
    
      cluster = DynoCluster(args.request_file, ips)
      cluster.launch()
      \# Run all the required tests
      cluster.teardown()
    
    This patch does this by forming clear relations between DynoCluster,
    DynoSpec, DynoNode and RedisNode.
    
    DynoCluster --manages--> list(DynoSpec), list(DynoNode)
    DynoNode    --manages--> RedisNode
    DynoNode and DynoSpec have a 1:1 relationship.
    
    In the future, we should decouple RedisNode and DynoNode and abstract
    away specific storage node classes to a generic class.
    Eg: 'RedisNode' implements 'StorageNode', and 'DynoNode' directly
    uses only 'StorageNode'.
    
    This patch also fixes a small bug where the incorrect port is used
    for the client listening port.
    
    This patch currently breaks other standalone scripts like func_test.py
    and large_value_test.py. This will be cleaned up in a future commit,
    and IMO it's not a serious issue as it's difficult to already use
    those scripts since they assume that a cluster with a very specific
    configuration is already setup (which is not that simple to do
    manually today).
    
    The initial motivation behind this patch is to create scripts to
    _only start_ or _only stop_ a dynomite cluster for the purpose
    of manual testing against a cluster. Before this patch,
    cluster_generator.py always cleaned up the resources on exit. Future
    commits will introduce new scripts like start_cluster.py and
    teardown_cluster.py which will allow developers to just start a cluster
    and run tests against it and then tear it down later. Other things
    that this patch enables is to do hypothetical experiments like
    dual running against 2 dynomite clusters with different
    configurations, expanding the framework to run only a subset of tests
    based on user flags passed to cluster_generator.py, etc.
    
    Future TODO: Add code comments to classes and methods as necessary.

--- test/dyno_node.py
+++ test/dyno_node.py
@@ -2,16 +2,49 @@
 import redis
 from node import Node
 from redis_node import RedisNode
-from dyno_cluster import DynoCluster
+
+from plumbum import BG
+from plumbum import local
+
+dynomite_bin = local.get('./test/_binaries/dynomite', 'src/dynomite')
 
 class DynoNode(Node):
-    def __init__(self, ip, port=8102,
-                 dnode_port=8101, data_store_port=1212):
-        super(DynoNode, self).__init__(ip, port)
+    #TODO: Decouple from Redis and make it more generic
+    def __init__(self, spec, seeds_list):
+        super(DynoNode, self).__init__(spec.ip, spec.port)
         self.name="Dyno" + self.name
         self.conf_file = None
-        self.dnode_port = dnode_port
-        self.data_store_node = RedisNode(ip, data_store_port)
+        self.spec = spec
+        self.seeds_list = seeds_list
+        self.dnode_port = spec.dnode_port
+        self.data_store_node = RedisNode(self.ip, spec.data_store_port)
+        self.logfile = 'logs/dynomite_{}.log'.format(self.ip)
+        self.proc_future = None
+
+    def launch(self):
+        # Write the configuration to a file according to 'self.spec'.
+        config_filename = self.spec.write_config(self.seeds_list)
+
+        # Launch the underlying data store process first.
+        self.data_store_node.launch()
+
+        # Launch the dynomite process
+        self.proc_future = dynomite_bin['-o', self.logfile, '-c', config_filename,
+            '-v6'] & BG(-9)
+
+    def teardown(self):
+        # First teardown the Dynomite node.
+        self.proc_future.proc.kill()
+        self.proc_future.wait()
+
+        # Lastly, teardown the storage node.
+        self.data_store_node.teardown()
+
+    def __enter__(self):
+        self.launch()
+
+    def __exit__(self, type_, value, traceback):
+        self.teardown()
 
     def get_connection(self):
         # should return the connection to the dyno port not the redis
