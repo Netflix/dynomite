808eb2fec1a9a8c0c6aa8e4180ac4141eb874e8f
Sailesh Mukil <smukil@netflix.com>
2018-11-11 17:20:34 -0800

    Refactor test framework to easily expand on cluster management
    This patch takes a lot of the code from test/cluster_generator.py
    and refactors it into easily manageable classes which allows for
    easily creating and destroying a dynomite cluster in a local test
    environment. Refer to the last paragraph of this commit message for
    the motivation behind this patch.
    
    To create a dynomite cluster, now it's as simple as doing the following:
    
      cluster = DynoCluster(args.request_file, ips)
      cluster.launch()
      \# Run all the required tests
      cluster.teardown()
    
    This patch does this by forming clear relations between DynoCluster,
    DynoSpec, DynoNode and RedisNode.
    
    DynoCluster --manages--> list(DynoSpec), list(DynoNode)
    DynoNode    --manages--> RedisNode
    DynoNode and DynoSpec have a 1:1 relationship.
    
    In the future, we should decouple RedisNode and DynoNode and abstract
    away specific storage node classes to a generic class.
    Eg: 'RedisNode' implements 'StorageNode', and 'DynoNode' directly
    uses only 'StorageNode'.
    
    This patch also fixes a small bug where the incorrect port is used
    for the client listening port.
    
    This patch currently breaks other standalone scripts like func_test.py
    and large_value_test.py. This will be cleaned up in a future commit,
    and IMO it's not a serious issue as it's difficult to already use
    those scripts since they assume that a cluster with a very specific
    configuration is already setup (which is not that simple to do
    manually today).
    
    The initial motivation behind this patch is to create scripts to
    _only start_ or _only stop_ a dynomite cluster for the purpose
    of manual testing against a cluster. Before this patch,
    cluster_generator.py always cleaned up the resources on exit. Future
    commits will introduce new scripts like start_cluster.py and
    teardown_cluster.py which will allow developers to just start a cluster
    and run tests against it and then tear it down later. Other things
    that this patch enables is to do hypothetical experiments like
    dual running against 2 dynomite clusters with different
    configurations, expanding the framework to run only a subset of tests
    based on user flags passed to cluster_generator.py, etc.
    
    Future TODO: Add code comments to classes and methods as necessary.

--- test/dyno_cluster.py
+++ test/dyno_cluster.py
@@ -1,10 +1,106 @@
 #!/usr/bin/env python3
+from collections import namedtuple
 import redis
 import random
+import yaml
+
+from dyno_node import DynoNode
+from utils import *
+
+DYN_O_MITE_DEFAULTS = dict(
+    secure_server_option='datacenter',
+    pem_key_file='conf/dynomite.pem',
+    data_store=0,
+    datastore_connections=1,
+)
+INTERNODE_LISTEN = 8101
+CLIENT_LISTEN = 8102
+REDIS_PORT = 1212
+STATS_PORT = 22222
+
+class DynoSpec(namedtuple('DynoSpec', 'ip port dc rack token '
+    'local_connections remote_connections seed_string')):
+    """Specifies how to launch a dynomite node"""
+
+    def __new__(cls, ip, port, rack, dc, token, local_connections,
+                remote_connections):
+        seed_string = '{}:{}:{}:{}:{}'.format(ip, port, rack, dc, token)
+        return super(DynoSpec, cls).__new__(cls, ip, port, rack, dc, token,
+            local_connections, remote_connections, seed_string)
+
+    def __init__(self, ip, port, rack, dc, token, local_connections,
+                 remote_connections):
+        self.dnode_port = INTERNODE_LISTEN
+        self.data_store_port = REDIS_PORT
+        self.stats_port = STATS_PORT
+
+    def _generate_config(self, seeds_list):
+        conf = dict(DYN_O_MITE_DEFAULTS)
+        conf['datacenter'] = self.dc
+        conf['rack'] = self.rack
+        dyn_listen = '{}:{}'.format(self.ip, self.dnode_port)
+        conf['dyn_listen'] = dyn_listen
+        conf['listen'] = '{}:{}'.format(self.ip, self.port)
+
+        # filter out our own seed string
+        conf['dyn_seeds'] = [s for s in seeds_list if s != self.seed_string]
+        conf['servers'] = ['{}:{}:0'.format(self.ip, self.data_store_port)]
+        conf['stats_listen'] = '{}:{}'.format(self.ip, self.stats_port)
+        conf['tokens'] = self.token
+        conf['local_peer_connections'] = self.local_connections
+        conf['remote_peer_connections'] = self.remote_connections
+        return dict(dyn_o_mite=conf)
+
+    def write_config(self, seeds_list):
+        config = self._generate_config(seeds_list)
+        filename = 'conf/{}:{}:{}.yml'.format(self.dc, self.rack, self.token)
+        with open(filename, 'w') as fh:
+            yaml.dump(config, fh, default_flow_style=False)
+        return filename
 
 class DynoCluster(object):
-    def __init__(self, nodes):
-        self.nodes = tuple(nodes)
+    def __init__(self, request_file, ips):
+        # Load the YAML file describing the cluster.
+        with open(request_file, 'r') as fh:
+            self.request = yaml.load(fh)
+        self.ips = ips
+        # Generate the specification for each node to be started in the cluster.
+        self.specs = list(self._generate_dynomite_specs())
+        self.nodes = []
+
+    def _generate_dynomite_specs(self):
+        tokens = tokens_for_cluster(self.request, None)
+        counts_by_rack = dict_request(self.request)
+        counts_by_dc = sum_racks(counts_by_rack)
+        total_nodes = sum(counts_by_dc.values())
+        for dc, racks in tokens:
+            dc_count = counts_by_dc[dc]
+            rack_count = counts_by_rack[dc]
+            remote_count = total_nodes - dc_count
+            for rack, tokens in racks:
+                local_count = rack_count[rack] - 1
+                for token in tokens:
+                    ip = next(self.ips)
+                    yield DynoSpec(ip, CLIENT_LISTEN, dc, rack, token,
+                        local_count, remote_count)
+
+    def launch(self):
+        # Get the list of seeds from the specification for each node.
+        seeds_list = [spec.seed_string for spec in self.specs]
+        # Launch each individual Dyno node.
+        self.nodes = [DynoNode(spec, seeds_list) for spec in self.specs]
+        for n in self.nodes:
+            n.launch()
+
+    def teardown(self):
+        for n in self.nodes:
+            n.teardown()
+
+    def __enter__(self):
+        self.launch()
+
+    def __exit__(self, type_, value, traceback):
+        self.teardown()
 
     def get_connection(self):
         node = random.choice(self.nodes)
--- test/utils.py
+++ test/utils.py
@@ -1,9 +1,71 @@
 #!/usr/bin/env python3
 import random
-import string 
+import string
+
+from ip_util import int2quad
+from ip_util import quad2int
+
+from itertools import count
+
+BASE_IPADDRESS = quad2int('127.0.1.1')
+RING_SIZE = 2**32
+
 
 def string_generator(size=6, chars=string.ascii_letters + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 def number_generator(size=4, chars=string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
+
+def dict_request(request):
+    """Converts the request into an easy to consume dict format."""
+
+    # We don't ingest the request in this format originally because dict
+    # ordering is nondeterministic, and one of our goals is to deterministically
+    # generate clusters.
+    # TODO: Python 3.6+ has ordered dicts, so the above doesn't hold true from that
+    # version.
+    return dict(
+        (dc['name'], dict(dc['racks']))
+        for dc in request
+    )
+
+def sum_racks(dcs):
+    return dict(
+        (name, sum(racks.values()))
+        for name, racks in dcs.items()
+    )
+
+def pick_tokens(count, start_offset):
+    stride = RING_SIZE // count
+    token = start_offset
+    for i in range(count):
+        yield token % RING_SIZE
+        token += stride
+
+def tokens_for_rack(count):
+    offset = random.randrange(0, RING_SIZE)
+    return list(pick_tokens(count, offset))
+
+def tokens_for_dc(racks):
+    return [
+        (name, tokens_for_rack(count))
+        for name, count in racks
+    ]
+
+def tokens_for_cluster(dcs, seed):
+    if seed is not None:
+        random.seed(seed)
+
+    return [
+        (dc['name'], tokens_for_dc(dc['racks']))
+        for dc in dcs
+    ]
+
+
+def dc_count(dc):
+    return sum(count for rack, count in dc)
+
+def generate_ips():
+    for ip in count(start=BASE_IPADDRESS, step=1):
+        yield int2quad(ip)
