Netflix/dynomite

commit 91e8fe38ea66b79b5241f02169fa04e2ad9a50ea

reduce compiler warnings

src/dyn_message.c
@@ -772,7 +772,7 @@ msg_payload_crc32(struct msg *rsp)
             }
         }
 
-        crc = crc32_sz((char *)start, end - start, crc);
+        crc = crc32_sz((char *)start, (size_t)(end - start), crc);
     }
     return crc;
 
@@ -899,10 +899,9 @@ msg_recv_chain(struct context *ctx, struct conn *conn, struct msg *msg)
     struct mbuf *mbuf;
     size_t msize;
     ssize_t n;
-
-    int expected_fill =
-        ((msg->dyn_parse_state == DYN_DONE || msg->dyn_parse_state == DYN_POST_DONE) &&
-         msg->dmsg->flags & 0x1) ? msg->dmsg->plen : -1;  //used in encryption case only
+    bool encryption_detected = (msg->dyn_parse_state == DYN_DONE ||
+                                 msg->dyn_parse_state == DYN_POST_DONE) &&
+                                (msg->dmsg->flags & 0x1);
 
     mbuf = STAILQ_LAST(&msg->mhdr, mbuf, next);
     /* This logic is unncessarily complicated. Ideally a connection should read
@@ -933,9 +932,9 @@ msg_recv_chain(struct context *ctx, struct conn *conn, struct msg *msg)
      *      b) mbuf is full till mbuf->end (mbuf_full) and we just decrypted that buffer.
      */
     if (mbuf == NULL ||
-        ((expected_fill == -1) && mbuf_full(mbuf)) ||
-        (expected_fill != -1 && mbuf->last == mbuf->end_extra) ||
-        (expected_fill != -1 && mbuf_full(mbuf) && (mbuf->flags & MBUF_FLAGS_JUST_DECRYPTED))) {
+        ((!encryption_detected) && mbuf_full(mbuf)) ||
+        (!encryption_detected && mbuf->last == mbuf->end_extra) ||
+        (!encryption_detected && mbuf_full(mbuf) && (mbuf->flags & MBUF_FLAGS_JUST_DECRYPTED))) {
         mbuf = mbuf_get();
         if (mbuf == NULL) {
             return DN_ENOMEM;
@@ -947,12 +946,10 @@ msg_recv_chain(struct context *ctx, struct conn *conn, struct msg *msg)
 
     ASSERT(mbuf->end_extra - mbuf->last > 0);
 
-    if (expected_fill == -1) {
+    if (!encryption_detected) {
         msize = mbuf_size(mbuf);
     } else {
-        msize = (msg->dmsg->plen <= mbuf->end_extra - mbuf->last) ?
-                                     msg->dmsg->plen :
-                                     mbuf->end_extra - mbuf->last;
+        msize = (size_t)MIN(msg->dmsg->plen, mbuf->end_extra - mbuf->last);
     }
 
     n = conn_recv_data(conn, mbuf->last, msize);
@@ -969,7 +966,7 @@ msg_recv_chain(struct context *ctx, struct conn *conn, struct msg *msg)
     msg->mlen += (uint32_t)n;
 
     //Only used in encryption case
-    if (expected_fill != -1) {
+    if (encryption_detected) {
         if ( n >=  msg->dmsg->plen  || mbuf->end_extra == mbuf->last) {
             //log_debug(LOG_VERB, "About to decrypt this mbuf as it is full or eligible!");
             struct mbuf *nbuf = NULL;
@@ -982,7 +979,8 @@ msg_recv_chain(struct context *ctx, struct conn *conn, struct msg *msg)
                     return DN_ENOMEM;
                 }
 
-                status = dyn_aes_decrypt(mbuf->start, mbuf->last - mbuf->start, nbuf, msg->owner->aes_key);
+                status = dyn_aes_decrypt(mbuf->start, (size_t)(mbuf->last - mbuf->start),
+                                         nbuf, msg->owner->aes_key);
                 if (status >= DN_OK) {
                     int remain = n - msg->dmsg->plen;
                     uint8_t *pos = mbuf->last - remain;

src/dyn_client.c
@@ -383,6 +383,7 @@ req_filter(struct context *ctx, struct conn *conn, struct msg *req)
     return false;
 }
 
+/*
 static void
 send_rsp_integer(struct context *ctx, struct conn *c_conn, struct msg *req)
 {
@@ -397,6 +398,7 @@ send_rsp_integer(struct context *ctx, struct conn *c_conn, struct msg *req)
     rstatus_t status = conn_event_add_out(c_conn);
     IGNORE_RET_VAL(status);
 }
+*/
 
 /* Expects req to be in the conn's outq already */
 void

src/dyn_conf.c
@@ -402,7 +402,7 @@ conf_dump(struct conf *cf)
     log_debug(LOG_VVERB, "  dyn_write_timeout: %d", cp->dyn_write_timeout);
     log_debug(LOG_VVERB, "  dyn_connections: %d", cp->dyn_connections);
 
-    log_debug(LOG_VVERB, "  gos_interval: %d", cp->gos_interval);
+    log_debug(LOG_VVERB, "  gos_interval: %lu", cp->gos_interval);
     log_debug(LOG_VVERB, "  conn_msg_rate: %d", cp->conn_msg_rate);
 
     log_debug(LOG_VVERB, "  secure_server_option: \"%.*s\"",
@@ -417,7 +417,7 @@ conf_dump(struct conf *cf)
             cp->write_consistency.len,
             cp->write_consistency.data);
 
-    log_debug(LOG_VVERB, "  stats_interval: %d", cp->stats_interval);
+    log_debug(LOG_VVERB, "  stats_interval: %lu", cp->stats_interval);
     log_debug(LOG_VVERB, "  stats_listen: %.*s",
             cp->stats_listen.pname.len, cp->stats_listen.pname.data);
 
@@ -1240,7 +1240,7 @@ static struct command conf_commands[] = {
 	  offsetof(struct conf_pool, stats_listen) },
 
 	{ string("stats_interval"),
-	  conf_set_string,
+	  conf_set_num,
 	  offsetof(struct conf_pool, stats_interval) },
 
     { string("enable_gossip"),

src/dyn_conf.h
@@ -99,7 +99,7 @@ struct conf_pool {
     int                dyn_connections;       /* dyn connections */  
     struct string      rack;                  /* this node's logical rack */
     struct array       tokens;                /* this node's token: dyn_token array */
-    int                gos_interval;          /* wake up interval in ms */
+    msec_t             gos_interval;          /* wake up interval in ms */
 
     /* none | datacenter | rack | all in order of increasing number of connections. (default is datacenter) */
     struct string      secure_server_option;
@@ -116,7 +116,7 @@ struct conf_pool {
     size_t             alloc_msgs_max;        /* allocated messages buffer size */
 
     /* stats info */
-    int                stats_interval;        /* stats aggregation interval */
+    msec_t             stats_interval;        /* stats aggregation interval */
     struct conf_listen stats_listen;          /* stats_listen: socket info for stats */
 
     /* connection pool details */

src/dyn_core.c
@@ -64,7 +64,6 @@ core_print_peer_status(void *arg1)
             }
         }
     }
-    schedule_task_1(core_print_peer_status, ctx, 30000);
 }
 
 void
@@ -470,7 +469,7 @@ core_timeout(struct context *ctx)
 
 		now = dn_msec_now();
 		if (now < then) {
-			int delta = (int)(then - now);
+			msec_t delta = (msec_t)(then - now);
 			ctx->timeout = MIN(delta, ctx->max_timeout);
 			return;
 		}

src/dyn_core.h
@@ -124,7 +124,7 @@ typedef int err_t;     /* error type */
 #include "entropy/dyn_entropy.h"
 
 #define ENCRYPTION 1
-typedef rstatus_t (*hash_func_t)(const char *, size_t, struct dyn_token *);
+typedef rstatus_t (*hash_func_t)(const unsigned char *, size_t, struct dyn_token *);
 
 typedef enum dyn_state {
 	INIT        = 0,
@@ -294,7 +294,7 @@ struct server_pool {
     int                backlog;              /* listen backlog */
     uint32_t           client_connections;   /* maximum # client connection */
     msec_t             server_retry_timeout_ms; /* server retry timeout in msec */
-    uint32_t           server_failure_limit; /* server failure limit */
+    uint8_t            server_failure_limit; /* server failure limit */
     unsigned           auto_eject_hosts:1;   /* auto_eject_hosts? */
     unsigned           preconnect:1;         /* preconnect? */
 
@@ -312,7 +312,7 @@ struct server_pool {
     struct string      rack;                 /* the rack for this node */
     struct array       tokens;               /* the DHT tokens for this server */
 
-    int                g_interval;           /* gossip interval */
+    msec_t             g_interval;           /* gossip interval */
     struct string      dc;                   /* server's dc */
     struct string      env;                  /* aws, network, etc */
     /* none | datacenter | rack | all in order of increasing number of connections. (default is datacenter) */
@@ -321,7 +321,7 @@ struct server_pool {
     struct string      recon_key_file;       /* file with Key encryption in reconciliation */
     struct string      recon_iv_file;        /* file with Initialization Vector encryption in reconciliation */
     struct endpoint    stats_endpoint;       /* stats_listen: socket info for stats */
-    int                stats_interval;       /* stats aggregation interval */
+    msec_t             stats_interval;       /* stats aggregation interval */
     bool               enable_gossip;        /* enable/disable gossip */
     size_t             mbuf_size;            /* mbuf chunk size */
     size_t             alloc_msgs_max;       /* allocated messages buffer size */

src/dyn_crypto.c
@@ -16,7 +16,7 @@
 #include "dyn_crypto.h"
 #include "dyn_server.h"
 
-static EVP_CIPHER *aes_cipher;
+static const EVP_CIPHER *aes_cipher;
 static RSA *rsa;
 static int rsa_size = 0;
 
@@ -39,7 +39,7 @@ static rstatus_t load_private_rsa_key_by_file(const struct string *pem_key_file)
         return DN_ERROR;
     }
 
-    unsigned char file_name[pem_key_file->len + 1];
+    char file_name[pem_key_file->len + 1];
     memcpy(file_name, pem_key_file->data, pem_key_file->len);
     file_name[pem_key_file->len] = '\0';
 
@@ -64,7 +64,7 @@ static rstatus_t load_private_rsa_key_by_file(const struct string *pem_key_file)
 
     rsa_size = RSA_size(rsa);
 
-    log_debug(LOG_INFO, "Private RSA structure filled");
+    log_debug(LOG_INFO, "Private RSA structure filled with size %d", rsa_size);
     /*
     char   *pri_key;           // Private key
     char   *pub_key;           // Public key
@@ -171,31 +171,12 @@ crypto_init(struct server_pool *sp)
 }
 
 rstatus_t
-crypto_init_for_test()
-{
-    THROW_STATUS(aes_init());
-
-    //init RSA
-    struct string pem_file = string("conf/dynomite.pem");
-    THROW_STATUS(load_private_rsa_key_by_file(&pem_file));
-
-    return DN_OK;
-}
-
-rstatus_t
 crypto_deinit(void)
 {
     EVP_CIPHER_CTX_cleanup(aes_encrypt_ctx);
-
-
     EVP_CIPHER_CTX_cleanup(aes_decrypt_ctx);
-
-
     free(aes_encrypt_ctx);
     free(aes_decrypt_ctx);
-
-    //free(aes_key);
-
     return DN_OK;
 }
 
@@ -207,19 +188,19 @@ base64_encode(const unsigned char *message, const size_t length)
     BIO *b64;
     FILE* stream;
 
-    int encodedSize = 4*ceil((double)length/3);
-    char *buffer = (char*)malloc(encodedSize+1);
+    size_t encodedSize = (size_t)(4 * ceil((double)length/3));
+    char *buffer = (char*)malloc(encodedSize + 1);
     if(buffer == NULL) {
         fprintf(stderr, "Failed to allocate memory\n");
         exit(1);
     }
 
-    stream = fmemopen(buffer, encodedSize+1, "w");
+    stream = fmemopen(buffer, (size_t)encodedSize + 1, "w");
     b64 = BIO_new(BIO_f_base64());
     bio = BIO_new_fp(stream, BIO_NOCLOSE);
     bio = BIO_push(b64, bio);
     BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL);
-    BIO_write(bio, message, length);
+    BIO_write(bio, message, (int)length);
     (void)BIO_flush(bio);
     BIO_free_all(bio);
     fclose(stream);
@@ -227,8 +208,22 @@ base64_encode(const unsigned char *message, const size_t length)
     return buffer;
 }
 
+/*
+static int
+calc_decode_length(const char *b64input, const size_t length)
+{
+    int padding = 0;
 
-int
+    // Check for trailing '=''s as padding
+    if(b64input[length-1] == '=' && b64input[length-2] == '=')
+        padding = 2;
+    else if (b64input[length-1] == '=')
+        padding = 1;
+
+    return (int)length*0.75 - padding;
+}
+
+static int
 base64_decode(const char *b64message, const size_t length, unsigned char **buffer)
 {
     BIO *bio;
@@ -254,44 +249,31 @@ base64_decode(const char *b64message, const size_t length, unsigned char **buffe
 
     return decodedLength;
 }
-
-
-int
-calc_decode_length(const char *b64input, const size_t length) {
-    int padding = 0;
-
-    // Check for trailing '=''s as padding
-    if(b64input[length-1] == '=' && b64input[length-2] == '=')
-        padding = 2;
-    else if (b64input[length-1] == '=')
-        padding = 1;
-
-    return (int)length*0.75 - padding;
-}
+*/
 
 rstatus_t
-aes_encrypt(const unsigned char *msg, size_t msg_len, unsigned char **enc_msg, unsigned char *aes_key)
+aes_encrypt(const unsigned char *msg, size_t msg_len, unsigned char **enc_msg, unsigned char *arg_aes_key)
 {
-    size_t block_len  = 0;
-    size_t enc_msg_len = 0;
+    int block_len  = 0;
+    int enc_msg_len = 0;
 
     *enc_msg = (unsigned char*)malloc(msg_len + AES_BLOCK_SIZE);
     if(enc_msg == NULL)
         return DN_ERROR;
 
-    //if(!EVP_EncryptInit_ex(aes_encrypt_ctx, aes_cipher, NULL, aes_key, aes_iv)) {
-    if(!EVP_EncryptInit_ex(aes_encrypt_ctx, aes_cipher, NULL, aes_key, aes_key)) {
+    //if(!EVP_EncryptInit_ex(aes_encrypt_ctx, aes_cipher, NULL, arg_aes_key, aes_iv)) {
+    if(!EVP_EncryptInit_ex(aes_encrypt_ctx, aes_cipher, NULL, arg_aes_key, arg_aes_key)) {
         log_debug(LOG_VERB, "This is bad data in EVP_EncryptInit_ex : '%.*s'", msg_len, msg);
         return DN_ERROR;
     }
 
-    if(!EVP_EncryptUpdate(aes_encrypt_ctx, *enc_msg, (int*)&block_len, (unsigned char*)msg, msg_len)) {
+    if(!EVP_EncryptUpdate(aes_encrypt_ctx, *enc_msg, &block_len, (unsigned char*)msg, (int)msg_len)) {
         log_debug(LOG_VERB, "This is bad data in EVP_EncryptUpdate : '%.*s'", msg_len, msg);
         return DN_ERROR;
     }
     enc_msg_len += block_len;
 
-    if(!EVP_EncryptFinal_ex(aes_encrypt_ctx, *enc_msg + enc_msg_len, (int*) &block_len)) {
+    if(!EVP_EncryptFinal_ex(aes_encrypt_ctx, *enc_msg + enc_msg_len, &block_len)) {
         log_debug(LOG_VERB, "This is bad data in EVP_EncryptFinal_ex : '%.*s'", msg_len, msg);
         return DN_ERROR;
     }
@@ -303,27 +285,26 @@ aes_encrypt(const unsigned char *msg, size_t msg_len, unsigned char **enc_msg, u
 
 
 rstatus_t
-dyn_aes_encrypt(const unsigned char *msg, size_t msg_len, struct mbuf *mbuf, unsigned char *aes_key)
+dyn_aes_encrypt(const unsigned char *msg, size_t msg_len, struct mbuf *mbuf, unsigned char *arg_aes_key)
 {
-
-    size_t block_len  = 0;
-    size_t enc_msg_len = 0;
+    int block_len  = 0;
+    int enc_msg_len = 0;
 
     ASSERT(mbuf != NULL && mbuf->last == mbuf->pos);
 
-    //if(!EVP_EncryptInit_ex(aes_encrypt_ctx, aes_cipher, NULL, aes_key, aes_iv)) {
-    if(!EVP_EncryptInit_ex(aes_encrypt_ctx, aes_cipher, NULL, aes_key, aes_key)) {
+    //if(!EVP_EncryptInit_ex(aes_encrypt_ctx, aes_cipher, NULL, arg_aes_key, aes_iv)) {
+    if(!EVP_EncryptInit_ex(aes_encrypt_ctx, aes_cipher, NULL, arg_aes_key, arg_aes_key)) {
         loga_hexdump(msg, msg_len, "Bad data in EVP_EncryptInit_ex, crypto data with %ld bytes of data", msg_len);
         return DN_ERROR;
     }
 
-    if(!EVP_EncryptUpdate(aes_encrypt_ctx, mbuf->start, (int*)&block_len, (unsigned char*) msg, msg_len)) {
+    if(!EVP_EncryptUpdate(aes_encrypt_ctx, mbuf->start, &block_len, (unsigned char*) msg, (int)msg_len)) {
         loga_hexdump(msg, msg_len, "Bad data in EVP_EncryptUpdate, crypto data with %ld bytes of data", msg_len);
         return DN_ERROR;
     }
     enc_msg_len += block_len;
 
-    if(!EVP_EncryptFinal_ex(aes_encrypt_ctx, mbuf->start + enc_msg_len, (int*) &block_len)) {
+    if(!EVP_EncryptFinal_ex(aes_encrypt_ctx, mbuf->start + enc_msg_len, &block_len)) {
         loga_hexdump(msg, msg_len, "Bad data in EVP_EncryptFinal_ex, crypto data with %ld bytes of data", msg_len);
         return DN_ERROR;
     }
@@ -342,7 +323,7 @@ dyn_aes_encrypt(const unsigned char *msg, size_t msg_len, struct mbuf *mbuf, uns
 
 
 rstatus_t
-dyn_aes_decrypt(unsigned char *enc_msg, size_t enc_msg_len, struct mbuf *mbuf, unsigned char *aes_key)
+dyn_aes_decrypt(unsigned char *enc_msg, size_t enc_msg_len, struct mbuf *mbuf, unsigned char *arg_aes_key)
 {
     if (ENCRYPTION) {
         size_t dec_len   = 0;
@@ -350,8 +331,8 @@ dyn_aes_decrypt(unsigned char *enc_msg, size_t enc_msg_len, struct mbuf *mbuf, u
 
         ASSERT(mbuf != NULL && mbuf->start == mbuf->pos);
 
-        //if(!EVP_DecryptInit_ex(aes_decrypt_ctx, aes_cipher, NULL, aes_key, aes_iv)) {
-        if(!EVP_DecryptInit_ex(aes_decrypt_ctx, aes_cipher, NULL, aes_key, aes_key)) {
+        //if(!EVP_DecryptInit_ex(aes_decrypt_ctx, aes_cipher, NULL, arg_aes_key, aes_iv)) {
+        if(!EVP_DecryptInit_ex(aes_decrypt_ctx, aes_cipher, NULL, arg_aes_key, arg_aes_key)) {
             loga_hexdump(enc_msg, enc_msg_len, "Bad data in EVP_DecryptInit_ex, crypto data with %ld bytes of data", enc_msg_len);
             return DN_ERROR;
         }
@@ -384,10 +365,10 @@ dyn_aes_decrypt(unsigned char *enc_msg, size_t enc_msg_len, struct mbuf *mbuf, u
  *
  */
 rstatus_t
-dyn_aes_encrypt_msg(struct msg *msg, unsigned char *aes_key)
+dyn_aes_encrypt_msg(struct msg *msg, unsigned char *arg_aes_key)
 {
     struct mhdr mhdr_tem;
-    size_t count = 0;
+    int count = 0;
 
     if (STAILQ_EMPTY(&msg->mhdr)) {
         return DN_ERROR;
@@ -409,7 +390,7 @@ dyn_aes_encrypt_msg(struct msg *msg, unsigned char *aes_key)
             return DN_ERROR;
         }
 
-        int n = dyn_aes_encrypt(mbuf->pos, mbuf->last - mbuf->pos, nbuf, aes_key);
+        int n = dyn_aes_encrypt(mbuf->pos, mbuf_length(mbuf), nbuf, arg_aes_key);
         if (n > 0)
             count += n;
 
@@ -439,7 +420,8 @@ dyn_aes_encrypt_msg(struct msg *msg, unsigned char *aes_key)
 
 
 rstatus_t
-aes_decrypt(unsigned char *enc_msg, size_t enc_msg_len, unsigned char **dec_msg, unsigned char *aes_key) {
+aes_decrypt(unsigned char *enc_msg, size_t enc_msg_len, unsigned char **dec_msg, unsigned char *arg_aes_key)
+{
     size_t dec_len   = 0;
     size_t block_len = 0;
 
@@ -447,8 +429,8 @@ aes_decrypt(unsigned char *enc_msg, size_t enc_msg_len, unsigned char **dec_msg,
     if(*dec_msg == NULL)
         return DN_ERROR;
 
-    //if(!EVP_DecryptInit_ex(aes_decrypt_ctx, aes_cipher, NULL, aes_key, aes_iv)) {
-    if(!EVP_DecryptInit_ex(aes_decrypt_ctx, aes_cipher, NULL, aes_key, aes_key)) {
+    //if(!EVP_DecryptInit_ex(aes_decrypt_ctx, aes_cipher, NULL, arg_aes_key, aes_iv)) {
+    if(!EVP_DecryptInit_ex(aes_decrypt_ctx, aes_cipher, NULL, arg_aes_key, arg_aes_key)) {
         log_debug(LOG_VERB, "This is bad data in EVP_DecryptInit_ex : '%.*s'", enc_msg_len, enc_msg);
         return DN_ERROR;
     }

src/dyn_crypto.h
@@ -28,12 +28,9 @@
 
 
 rstatus_t crypto_init(struct server_pool *sp);
-rstatus_t crypto_init_for_test(void);
 rstatus_t crypto_deinit(void);
 
 char* base64_encode(const unsigned char *message, const size_t length);
-int base64_decode(const char *b64message, const size_t length, unsigned char **buffer);
-int calc_decode_length(const char *b64input, const size_t length);
 
 rstatus_t aes_encrypt(const unsigned char *msg, size_t msgLen, unsigned char **encMsg, unsigned char *aes_key);
 rstatus_t aes_decrypt(unsigned char *encMsg, size_t encMsgLen, unsigned char **decMsg, unsigned char *aes_key);

commit 9440c96893fb857c2c522b653a80b54db6b78aaa

Allow multi-buffer payload decryption
In many cases, queries or responses that spanned multiple mbufs
would not get decrypted correctly causing cross-DC traffic to be
dropped silently which resulted in a failure to replicate data in
different DCs.

After a lot of debugging, this was found to be due to a typo
introduced in a previous commit:
https://github.com/Netflix/dynomite/commit/91e8fe38ea66b79b5241f02169fa04e2ad9a50ea

This typo is now fixed and multi-buffer decryption works seamlessly.

src/dyn_message.c
@@ -908,8 +908,8 @@ static rstatus_t msg_recv_chain(struct context *ctx, struct conn *conn,
    * buffer.
    */
   if (mbuf == NULL || ((!encryption_detected) && mbuf_full(mbuf)) ||
-      (!encryption_detected && mbuf->last == mbuf->end_extra) ||
-      (!encryption_detected && mbuf_full(mbuf) &&
+      (encryption_detected && mbuf->last == mbuf->end_extra) ||
+      (encryption_detected && mbuf_full(mbuf) &&
        (mbuf->flags & MBUF_FLAGS_JUST_DECRYPTED))) {
     mbuf = mbuf_get();
     if (mbuf == NULL) {