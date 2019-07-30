ab3cfea3d677f7c5a3703478023568c97b626044
Miguel Filipe <miguel.filipe@gmail.com>
2019-07-30 22:35:47 +0100

    add dynomit-three-rack-config

--- dynomite_three_rack_config.sh
+++ dynomite_three_rack_config.sh
@@ -4,6 +4,8 @@ set -ex
 # this is a 3-AZ (3-rack in dynomite lingo) setup.
 # I'm using non-standard port numbers purposefully to avoid conflicting with other configs by error
 
+ulimit -n 32000  # increase file descriptor limit to 32k
+
 redis-server --port 33211 &
 src/dynomite --conf-file=conf/msf_redis_node1.yml &
 
